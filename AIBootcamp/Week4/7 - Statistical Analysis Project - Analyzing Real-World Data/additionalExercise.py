90e664b4894b02729688056203991f33345c8011
divecx <antoniusbodil13@gmail.com>
2025-01-05 17:21:55 +0700

    Statistical Analysis Project - Analyzing Real-World Data - additionalExercise

---
+++ AIBootcamp/Week4/7 - Statistical Analysis Project - Analyzing Real-World Data/additionalExercise.py
@@ -0,0 +1,5 @@
+# 1. extend the project by exploring additional relationships (e.g., day of the week vs tip amount)
+
+# 2. perform multiple linear regression with additional variables (e.g., include smoking status)
+
+# 3. use another real-world dataset(e.g., healthcare or sales data) to apply similiar techniques
