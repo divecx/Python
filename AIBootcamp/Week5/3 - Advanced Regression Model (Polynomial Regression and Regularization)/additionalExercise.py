6bc8d4538dd7960f4f333fdbb5ec0fa9032f91fc
divecx <antoniusbodil13@gmail.com>
2025-01-05 19:04:05 +0700

    Advanced Regression Model (Polynomial Regression and Regularization) - additionalExercise

---
+++ AIBootcamp/Week5/3 - Advanced Regression Model (Polynomial Regression and Regularization)/additionalExercise.py
@@ -0,0 +1,9 @@
+# 1. vary regularization parameter:
+#         - experiment with different values of alpha (e.g., 0.1, 1, 10) for ridge and lasso regression
+#         - observe how the model's coefficients and performance change
+
+# 2. use multiple features:
+#         - include more features(e.g., HouseAge, AveRooms) and observe the impact on model performance
+        
+# 3. Feature importance with lasso:
+#         - use lasso regression to perform feature selection and identify the most relevant predictor
