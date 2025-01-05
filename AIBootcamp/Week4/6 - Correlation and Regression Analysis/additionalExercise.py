0341dd92bb08f79c5b768115c64e079771a55211
divecx <antoniusbodil13@gmail.com>
2025-01-05 16:44:20 +0700

    Correlation and Regression Analysis - additionalExercise

---
+++ AIBootcamp/Week4/6 - Correlation and Regression Analysis/additionalExercise.py
@@ -0,0 +1,6 @@
+# 1. fit a multiple linear regression model with multiple independent variables
+
+# 2. compare correlation and regression result for non-linear realtionships
+
+# 3. use real-world datasets (e.g., housing prices) for regression analysis
+
