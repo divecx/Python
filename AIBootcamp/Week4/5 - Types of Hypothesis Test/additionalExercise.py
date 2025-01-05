43f54f252f2e7976633f80ea4747df1a1d5bc317
divecx <antoniusbodil13@gmail.com>
2025-01-05 16:18:25 +0700

    Types of Hypothesis Test - additionalExercise

---
+++ AIBootcamp/Week4/5 - Types of Hypothesis Test/additionalExercise.py
@@ -0,0 +1,5 @@
+# 1. perform a two-way ANOVA to test for interaction effects
+
+# 2. use real-world datasets (e.g., student scores by gender and class) for hypothesis testing
+
+# 3. visualize test result using boxplots or bar plots
