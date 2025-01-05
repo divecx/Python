92c3a283a60732e829cba2854272ea197dd427c7
divecx <antoniusbodil13@gmail.com>
2025-01-05 09:38:36 +0700

    Probability Disributions in Machine Learning - additionalExercise

---
+++ AIBootcamp/Week4/2 - Probability Disributions in Machine Learning/additonalExercise.py
@@ -0,0 +1,6 @@
+# 1. compare the effects of skewness and kurtosis on different datasets
+
+# 2. simulate random variables from custom distributions
+
+# 3. explore datasets with real-world applications of distributions
+
