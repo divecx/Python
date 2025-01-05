cee96fbfc6a8a45915abe9457b1447cc58946004
divecx <antoniusbodil13@gmail.com>
2025-01-05 12:37:26 +0700

    Hypothesis Testing and P-Values - additionalExercise

---
+++ AIBootcamp/Week4/4 - Hypothesis Testing and P-Values/additionalExercise.py
@@ -0,0 +1,5 @@
+# 1. perform a z-test for large sample sizes
+
+# 2. use the iris dataset to set if the mean sepal length differs between two species
+
+# 3. perform hypothesis testing on proportions using the binomial distribution
