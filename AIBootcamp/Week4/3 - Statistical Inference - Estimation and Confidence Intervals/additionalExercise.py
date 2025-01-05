f19da27a8889ad117c8bca9f40887f0daa701562
divecx <antoniusbodil13@gmail.com>
2025-01-05 09:57:22 +0700

    Statistical Inference - Estimation and Confidence Intervals - additionalExercise

---
+++ AIBootcamp/Week4/3 - Statistical Inference - Estimation and Confidence Intervals/additionalExercise.py
@@ -0,0 +1,5 @@
+# 1. Create confidence intervals for other statistics
+
+# 2. Perform stratified sampling and compoare intervals across strata
+
+# 3. Visualze confidence intervals for multiple samples using Matplotlib
